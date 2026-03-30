dadeb73256756a1199c9818fe3858152e522be02
xu4nphu <phuvo416@gmail.com>
2026-03-30 07:55:28 +0700

    Khởi tạo móng dự án Bo Gear, cấu hình xong Neon DB và README

---
+++ src/main/java/com/bogear/BogearApplication.java
@@ -0,0 +1,13 @@
+package com.bogear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BogearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BogearApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bogear/BogearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bogear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BogearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
